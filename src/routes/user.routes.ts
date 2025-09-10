74d7ffc228fe8d72ec796b78df3e18dd8e362271
Felipe Boff <felipe.boff3@gmail.com>
2025-09-10 20:01:31 -0300

    feat(user): add params validation to post route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,20 @@ import type { FastifyInstance } from "fastify";
 import { UserFactory } from "../modules/users/user.factory";
 import { zodMiddleware } from "../shared/middleware/zod-validator";
 import { createUserSchema } from "../modules/users/user.dto";
+import z from "zod";
+
+const userParamsValidator = z.object({
+  id: z.uuid("id da requisição inválido").optional(),
+});
 
 export async function userRoutes(app: FastifyInstance) {
   app.post(
     "/:id",
-    { preHandler: [zodMiddleware({ body: createUserSchema })] },
+    {
+      preHandler: [
+        zodMiddleware({ body: createUserSchema, params: userParamsValidator }),
+      ],
+    },
     UserFactory.findById
   );
 }
