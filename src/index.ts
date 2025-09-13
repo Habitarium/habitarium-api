2234965a85b07a151ecc40b1f10b50d2d76e52b5
yoruwitch <evelyn.fernandes4@gmail.com>
2025-09-13 09:36:53 -0300

    fix: add fastify CORS plugin

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import Fastify from "fastify";
 import { registerRoutes } from "./routes/index.routes";
 import { fastifyErrorHandler } from "./utils/error-handler";
+import cors from "@fastify/cors";
 
 async function main() {
   const port = Number(process.env.API_PORT ?? 3000);
   const host = process.env.API_HOST ?? "0.0.0.0";
 
   const app = Fastify();
+
+  // Registre o plugin de CORS aqui, antes das rotas
+  // Isso permite que o backend receba requisições de diferentes origens.
+  await app.register(cors, {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+   
+  });
 
   app.setErrorHandler(fastifyErrorHandler);
 
